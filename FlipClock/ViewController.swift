1d86286f4027253c4f6bf0cd37099298082661b8
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-06-25 19:44:00 +0900

    프로젝트 시작

---
+++ FlipClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlipClock
+//
+//  Created by 홍승현 on 2022/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
