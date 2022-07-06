bb04b9f20c599660797fae26e76dc63509f64efb
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-07-06 19:35:19 +0900

    Feat: SettingsViewController 추가

---
+++ FlipClock/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  FlipClock
+//
+//  Created by 홍승현 on 2022/07/06.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
