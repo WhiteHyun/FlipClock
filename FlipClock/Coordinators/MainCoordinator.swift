0b752205d4fe210629ec18db40f97e738d2dd03f
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-07-06 20:14:04 +0900

    Feat: child와 parent process 구현

--- FlipClock/Coordinators/MainCoordinator.swift
+++ FlipClock/Coordinators/MainCoordinator.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 
-class MainCoordinator: Coordinator {
+class MainCoordinator: NSObject, Coordinator {
   
   var childCoordinators: [Coordinator] = []
   var navigationController: UINavigationController
@@ -19,11 +19,56 @@ class MainCoordinator: Coordinator {
   }
   
   func start() {
+    
+    navigationController.delegate = self
+    
     let vc = ViewController()
     vc.coordinator = self
     navigationController.pushViewController(vc, animated: false)
-    
   }
   
   
+  func moveToSetting() {
+    let child = SettingCoordinator(navigationController: navigationController)
+    
+    child.parentCoordinator = self
+    childCoordinators.append(child)
+    child.start()
+  }
+  
+  
+  func childDidFinish(_ child: Coordinator?) {
+    for (index, coordinator) in childCoordinators.enumerated() {
+      if coordinator === child {
+        childCoordinators.remove(at: index)
+        break
+      }
+    }
+  }
+}
+
+
+// MARK: - UINavigationControllerDelegate
+
+extension MainCoordinator: UINavigationControllerDelegate {
+  func navigationController(
+    _ navigationController: UINavigationController,
+    didShow viewController: UIViewController,
+    animated: Bool
+  ) {
+    
+    guard let fromVC = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+      return
+    }
+    
+    if navigationController.viewControllers.contains(fromVC) {
+      return
+    }
+    
+    if let settingVC = fromVC as? SettingsViewController {
+      childDidFinish(settingVC.coordinator)
+    }
+    
+    
+  }
 }
