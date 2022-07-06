6b9cf04d20006cf8f607cd1784a63b3a90e438d9
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-07-06 13:54:25 +0900

    Feat: FlipClockView 클래스 추가

---
+++ FlipClock/FlipClockView.swift
@@ -0,0 +1,32 @@
+//
+//  FlipClockView.swift
+//  FlipClock
+//
+//  Created by 홍승현 on 2022/07/06.
+//
+
+import UIKit
+
+class FlipClockView: UIView {
+  
+  // MARK: - Initialization
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
+
+// MARK: - Configuration
+
+extension FlipClockView {
+  
+  func configure() {
+    
+  }
+  
+}
