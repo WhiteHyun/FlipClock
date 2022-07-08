1b65e2b5c06049b6b6814fc01ba9ba6aa330e972
홍승현 <whi7ehyun@gmail.com>
2022-07-08 13:17:07 +0900

    v0.1.0 (#4)
    * Test: 테스트 버전
    
    * Chore: SnapKit 추가
    
    * Chore: Then 프레임워크 추가
    
    * Feat: FlipLabel SnapKit Constraint 적용
    
    * Feat: Then 적용
    
    * Test: 테스트 코드 추가
    
    * Feat: 코드 구조화
    
    * Style: 메소드명 변경
    
    * Style: 주석 추가
    
    * Feat: configure 메서드 추가
    
    - 초기화 과정에 필수적으로 적용할 코드를 메서드로 구조화함.
    
    * Style: 메서드명 변경
    
    createSnapshotViews -> createLabelImages
    
    * Style: 클래스명 변경
    
    FlippableLabel -> Flipitem
    
    * Feat: 가운데 구분선 추가
    
    * Feat: FlipView 파일 추가
    
    * Feat: 시간, 분, 초에 따른 Enum타입 생성
    
    * Feat: 중간 구분선 FlipView 클래스로 이동
    
    * Feat: time 프로퍼티 추가
    
    * Feat: FlipClockView 클래스 추가
    
    * Feat: 시간, 분, 초 단위의 FlipClock 생성
    
    * Feat: Constraint 적용
    
    * Feat: Timer 추가
    
    * Feat: 같은 값인 경우 단순반환
    
    * Feat: FlipView에 적용
    
    * Test: 테스트코드 작성
    
    * Feat: 화면 회전할 때 스택뷰 축 변경
    
    * Fix: stackview의 축이 바뀔 때 cliptobounds값이 바뀌는 현상 수정
    
    * Refactor: 코드 역할에 맞게 addSubviews 코드 이동
    
    * Feat: Coordinator로 실행 구성
    
    * Feat: SettingsViewController 추가
    
    * Feat: SettingCoordinator 추가
    
    * Feat: child와 parent process 구현
    
    * Fix: 화면 회전할 때 background 문제 생기는 버그 수정
    
    * Fix: 백그라운드 진입시 생기는 Flip 버그 수정
    
    - 사라져야되는 텍스트가 사라지지 않는 버그
    
    * Feat: 우측 navbar button 추가
    
    - gear 아이콘으로 설정
    - 설정페이지로 넘어가는 objc func 선언
    
    * Feat: 시계 왼 오른쪽 간격 40으로 설정
    
    * Feat: "준비중" 문구 표시
    
    * Feat: 기본 테이블뷰 설정
    
    * Style: 앱 아이콘 추가

--- FlipClock/SceneDelegate.swift
+++ FlipClock/SceneDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
+  var coordinator: MainCoordinator?
 
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -17,10 +18,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     guard let scene = (scene as? UIWindowScene) else { return }
-    
     window = UIWindow(windowScene: scene)
-    window?.rootViewController = ViewController()
+    
+    let vc = UINavigationController()
+    coordinator = MainCoordinator(navigationController: vc)
+    
+    window?.rootViewController = vc
     window?.makeKeyAndVisible()
+    
+    coordinator?.start()
   }
 
 }
