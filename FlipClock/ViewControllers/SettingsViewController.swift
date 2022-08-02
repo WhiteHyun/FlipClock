b3a7d6eaee118f2e92122237a16a3c204d215873
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-08-02 14:48:41 +0900

    Refactor: 테스트 코드 제거

--- FlipClock/ViewControllers/SettingsViewController.swift
+++ FlipClock/ViewControllers/SettingsViewController.swift
@@ -27,18 +27,6 @@ extension SettingsViewController {
   
   private func configureUI() {
     view.backgroundColor = .lightGray
-    
-    let label = UILabel().then {
-      $0.textAlignment = .center
-      $0.text = "준비중이에요!"
-      $0.textColor = .darkGray
-      $0.font = .systemFont(ofSize: 30, weight: .semibold)
-    }
-    view.addSubview(label)
-    
-    label.snp.makeConstraints { make in
-      make.centerX.centerY.equalToSuperview()
-    }
   }
   
   private func configureTableView() {
