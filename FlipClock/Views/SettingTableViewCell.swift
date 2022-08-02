f328687457a6392425ab46f2981a91c320728eb3
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-08-02 15:38:17 +0900

    Refactor: configure 메서드 제거

--- FlipClock/Views/SettingTableViewCell.swift
+++ FlipClock/Views/SettingTableViewCell.swift
@@ -11,17 +11,11 @@ class SettingTableViewCell: UITableViewCell {
   
   static let id = "SettingTableViewCell"
   
-  
-  
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
-  }
-  
-  public func configure() {
-    accessoryType = .disclosureIndicator
   }
 }
