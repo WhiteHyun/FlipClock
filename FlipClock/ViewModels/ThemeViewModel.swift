83e339210a4b6d9212a1c6bea5e90d20da3e838e
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-08-03 23:14:34 +0900

    Feat: 색상 설정 프로퍼티 추가

--- FlipClock/ViewModels/ThemeViewModel.swift
+++ FlipClock/ViewModels/ThemeViewModel.swift
@@ -10,6 +10,9 @@ import UIKit
 struct ThemeViewModel {
   
   let clockThemes: [UIImage?] = [.init(named: "theme1"), .init(named: "theme2")]
+  let backgroundColor: [UIColor] = [.lightGray, .init(red: 0.941, green: 0.949, blue: 0.969, alpha: 1)]
+  let textColor: [UIColor] = [.white, .black]
+  let clockBackgroundColor: [UIColor] = [.black, .white]
   
   var numberOfRowsInSection: Int {
     clockThemes.count
