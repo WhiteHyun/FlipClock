27c02e99ab4459d3dd072b6dd74b5b8f900906e6
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-08-03 12:35:40 +0900

    Feat: ThemeTableViewCell 추가

---
+++ FlipClock/Views/ThemeTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  ThemeTableViewCell.swift
+//  FlipClock
+//
+//  Created by 홍승현 on 2022/08/03.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ThemeTableViewCell: UITableViewCell {
+  
+  static let id = "ThemeTableViewCell"
+  
+  private lazy var themeImageView = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+  }
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configure() {
+    contentView.addSubview(themeImageView)
+    
+    themeImageView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+  }
+}
