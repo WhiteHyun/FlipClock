50a17c43708fb2558be892670cfb2d1735cd23b4
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-08-13 12:07:59 +0900

    Feat: cell accessory 타입을 arrow indicator로 변경

--- FlipClock/Views/SettingTableViewCell.swift
+++ FlipClock/Views/SettingTableViewCell.swift
@@ -13,6 +13,7 @@ class SettingTableViewCell: UITableViewCell {
   
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
+    accessoryType = .disclosureIndicator
   }
   
   required init?(coder: NSCoder) {
