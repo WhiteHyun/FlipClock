20125f77e5fed3d1b7110595a1322326b63e0c1a
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-09-15 01:12:15 +0900

    Refactor: TableView rx 적용

--- FlipClock/Views/ThemeVC/ThemeViewModel.swift
+++ FlipClock/Views/ThemeVC/ThemeViewModel.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
+import RxSwift
+import Then
+
 struct ThemeViewModel {
 
-  let clockThemes: [UIImage?] = [.init(named: "theme1"), .init(named: "theme2")]
+  private let clockThemes = [UIImage(named: "theme1"), UIImage(named: "theme2")]
   private let backgroundColorTheme = [0xAAAAAA, 0xF0F2F7]
   private let textColorTheme = [0xFFFFFF, 0x000000]
   private let clockBackgroundColorTheme = [0x000000, 0xFFFFFF]
 
-  var numberOfRowsInSection: Int {
-    clockThemes.count
+  var data: Observable<[UIImage?]> {
+    return Observable.just(clockThemes)
   }
 
   func userDefaults(storeWith index: Int) {
