83eab8e8977cb7e5efe9948fa823a70a65d22620
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-20 13:07:42 +0900

    Feat: HeaderView
    - 시계 인스턴스 추가

--- FlipClock/Views/ThemeVC/ThemeHeaderView.swift
+++ FlipClock/Views/ThemeVC/ThemeHeaderView.swift
@@ -7,17 +7,54 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class ThemeHeaderView: UITableViewHeaderFooterView {
 
   static let id = "ThemeHeaderView"
 
+  private let containerView = UIStackView().then {
+    $0.alignment = .center
+    $0.distribution = .fillEqually
+    $0.spacing = 10
+  }
+
+  private let exampleView = [FlipView]().with {
+    for _ in 0..<3 {
+      let view = FlipView()
+      $0.append(view)
+    }
+  }
+
   override init(reuseIdentifier: String?) {
     super.init(reuseIdentifier: reuseIdentifier)
+    setupLayouts()
+    setupConstraints()
     setupStyles()
   }
 
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setupLayouts() {
+    addSubview(containerView)
+    exampleView.forEach {
+      containerView.addArrangedSubview($0)
+    }
+  }
+
+  private func setupConstraints() {
+    containerView.snp.makeConstraints { make in
+      make.edges.equalToSuperview().inset(30)
+    }
+
+    exampleView.forEach { view in
+      view.snp.makeConstraints { make in
+        make.height.equalTo(view.snp.width)
+      }
+    }
   }
 
   private func setupStyles() {
