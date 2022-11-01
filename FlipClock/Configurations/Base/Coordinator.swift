43865500cb704942d59d467547b75beb6abd41a4
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-01 23:44:04 +0900

    Add: Coordinator properties & method
    - parentCoordinator: 부모 코디네이터
    - start(child:): 자식 코디네이터를 편하게 실행시켜줄 수 있는 api
    - remove(child:): 자식 코디네이터를 안전하게 제거해줄 수 있는 api

--- FlipClock/Configurations/Base/Coordinator.swift
+++ FlipClock/Configurations/Base/Coordinator.swift
@@ -10,8 +10,24 @@ import UIKit
 protocol Coordinator: AnyObject {
 
   var childCoordinators: [Coordinator] { get set }
+  var parentCoordinator: Coordinator? { get set }
   var navigationController: UINavigationController { get set }
 
   func start()
+  func start(child: Coordinator)
+  func remove(child: Coordinator)
+}
 
+
+extension Coordinator {
+  
+  func start(child: Coordinator) {
+    self.childCoordinators.append(child)
+    child.parentCoordinator = self
+    child.start()
+  }
+  
+  func remove(child: Coordinator) {
+    self.childCoordinators = self.childCoordinators.filter { $0 !== child }
+  }
 }
